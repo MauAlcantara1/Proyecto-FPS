6439f36ac0cc6fe596c6b47a5e131e8d59deb825
Mar <156032938+MarMontejo04@users.noreply.github.com>
2026-08-22 20:32:20 -0600

    Script para persecucion por parte del enemigo, prueba de navmesh

--- Assets/scripts/Enemigo/EnemigoRango.cs
+++ Assets/scripts/Enemigo/EnemigoRango.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+
+public class EnemigoRango : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public NavMeshAgent enemigo;
+    public float velocidad;
+    public bool perseguir;
+    public float rango;
+    float distancia;
+
+    public Transform objetivo; 
+
+    private void Update()
     {
-        
+        distancia = Vector3.Distance(enemigo.transform.position, objetivo.position);
+
+        if(distancia < rango)
+        {
+            perseguir = true;
+        }else if(distancia > rango + 3)
+        {
+            perseguir = false;
+        }
+
+        if(perseguir == false)
+        {
+            enemigo.speed = 0;
+        }else if (perseguir == true)
+        {
+            enemigo.speed=velocidad;
+            enemigo.SetDestination(objetivo.position);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(enemigo.transform.position,rango);
     }
+    
 }
