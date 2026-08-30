1b4933be13ccd91f5632298c780804e5b763b9a2
Mar <156032938+MarMontejo04@users.noreply.github.com>
2026-08-29 23:15:21 -0600

    Spawn Enemigo Base V2
    Base del spawn prueba completa, lista para implementacion en mapa

--- Assets/scripts/Enemigo/EnemigoController.cs
+++ Assets/scripts/Enemigo/EnemigoController.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class EnemigoController : MonoBehaviour
+public class EnemigoRango : MonoBehaviour
 {
     public NavMeshAgent enemigo;
     private Transform objetivo; 
 
+    [Header("Vida")]
+    [SerializeField] private int vidaMaxima = 100;
+    private int vidaActual;
+    [SerializeField] private Puntuacion puntuacion;
+
+    [Header("Características")]
     public float velocidad;
     public float rango;
     float distancia;
     private HordaManager hordaManager;
 
 
-
     private void Start()
     {
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-
+        
         if (playerObject != null)
         {
             objetivo = playerObject.transform;
         }
 
         hordaManager = FindFirstObjectByType<HordaManager>();
+
+        vidaActual = vidaMaxima;
+
     }
 
     private void Update()
@@ -57,19 +65,48 @@ public class EnemigoController : MonoBehaviour
         }
     }
 
-    public void Muere()
-    {
-        if(hordaManager != null)
-        {
-            hordaManager.EnemyDied(gameObject);
-        }
-        Destroy(gameObject);
-    }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(enemigo.transform.position,rango);
+    }
+
+    public void RecibirDaño(int daño)
+    {
+        vidaActual -= daño;
+
+        if (puntuacion != null)
+        {
+            puntuacion.SumarPuntosPorDaño();
+        }
+
+
+        if (vidaActual <= 0)
+        {
+            Morir();
+        }
+    }
+
+    private void Morir()
+    {
+        Debug.Log("Zombie muerto");
+
+        if(hordaManager != null)
+        {
+            hordaManager.EnemyDied(gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            RecibirDaño(25);
+
+            Destroy(collision.gameObject);
+        }
     }
     
 }
